977b596ee0c0693e4a3d952229e01d1a90273cc0
amirreza <amirreza@fimo.io>
2024-07-23 23:18:45 +0330

    http server created

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("This is a CRUD application")
+	router := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("public"))
+	router.Handle("/public/*", http.StripPrefix("/public/", fileServer))
+
+	killSig := make(chan os.Signal, 1)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			slog.Error("server error", slog.Any("err", err))
+		}
+	}()
+
+	slog.Info("Server started...")
+	<-killSig
+
+	slog.Info("Shutting down server")
+
+	// Create a context with a timeout for shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Attempt to gracefully shut down the server
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server shutdown failed", slog.Any("err", err))
+		os.Exit(1)
+	}
+
+	slog.Info("Server shutdown complete")
 }
